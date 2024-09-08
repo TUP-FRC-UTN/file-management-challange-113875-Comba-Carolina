eb973a251babc14dd8abc5824e60ed9b04472c17
carolinacomba <113875@tecnicatura.frc.utn.edu.ar>
2024-09-08 13:41:16 -0300

    form terminado. faltan validaciones

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FileItem, FileOwner, FileType } from '../../../models/file.item.model';
+import { FILE_LIST, OWNERS } from '../../../data/file.storage';
 
 @Component({
   selector: 'app-form',
   standalone: true,
   imports: [],
   templateUrl: './form.component.html',
-  styleUrl: './form.component.css'
+  styleUrl: './form.component.css',
 })
-export class FormComponent {
+export class FormComponent implements OnInit {
+agregarDuenio() {
+throw new Error('Method not implemented.');
+}
+  ngOnInit(): void {
+    this.cargarCombo();
+  }
 
+  carpetasExistentes: FileItem[] = [];
+  files: FileItem[] = FILE_LIST;
+
+  duenios: FileOwner[] = OWNERS;
+
+  constructor() {}
+
+  cargarCombo() {
+    for (let i = 0; i < this.files.length; i++) {
+      if (this.files[i].type == FileType.FOLDER) {
+        this.carpetasExistentes.push(this.files[i]);
+      }
+    }
+  }
 }
